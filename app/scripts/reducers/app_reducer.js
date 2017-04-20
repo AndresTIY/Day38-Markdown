33600f4526fe0a4d6a1ee4b16f865927bc83cfd1
Andres Cifuentes <aacifuentes@gmail.com>
2017-04-20 15:37:56 -0500

    deleted vars

--- app/scripts/reducers/app_reducer.js
+++ app/scripts/reducers/app_reducer.js
@@ -1,10 +1,6 @@
 import store from "../store.js";
 import { createStore } from "redux";
 
-const url = "https://api.backendless.com/v1/data/md_note";
-const appId = "892747C4-CCC9-E96F-FF91-006B50E61400";
-const restKey = "30D82F23-700A-52A1-FF7B-1BC275C5F700";
-
 export default function AppReducer(state, action) {
   if (state === undefined) {
     return {
