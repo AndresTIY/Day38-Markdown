708b2679c0311d228934fef3e97d28bd002eea66
Andres Cifuentes <aacifuentes@gmail.com>
2017-04-19 20:38:30 -0500

    ajax request set up in reducer, working on clearing text area

--- app/scripts/reducers/app_reducer.js
+++ app/scripts/reducers/app_reducer.js
@@ -1,5 +1,10 @@
 import store from "../store.js";
 import { createStore } from "redux";
+import marked from "marked";
+
+const url = "https://api.backendless.com/v1/data/md_note";
+const appId = "892747C4-CCC9-E96F-FF91-006B50E61400";
+const restKey = "30D82F23-700A-52A1-FF7B-1BC275C5F700";
 
 export default function AppReducer(state, action) {
   if (state === undefined) {
@@ -15,7 +20,24 @@ export default function AppReducer(state, action) {
     case "SAVE_NOTE":
       //empty text area after saving to server
       console.log("state.text = ", state.text);
-      return state;
+      $.ajax({
+        url: url,
+        type: "POST",
+        headers: {
+          "application-id": appId,
+          "secret-key": restKey,
+          "Content-Type": "application/json",
+          "application-type": "REST"
+        },
+        data: JSON.stringify({
+          note: marked(state.text)
+        })
+      }).then((data, success) => {
+        console.log("note saved = ", data, success);
+      });
+      return Object.assign({}, state, {
+        isSent: true
+      });
 
     default:
       console.debug("Unhandled State");
